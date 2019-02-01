a60ca7305e69cf9d116a3560c9b8ce3cf54e7bf6
Brett Knights <brett@knightsofthenet.com>
2019-02-01 09:54:56 -0800

    Moved processHooks into own module

--- index.js
+++ index.js
@@ -14,65 +14,6 @@ module.exports = (function(){
 		});
 	}
 
-	function processHooks(shopConfig, makeActive, appHookTopics, opts) {
-		var hookURL = 'https://' + opts.hostName + opts.path + '/hooks';
-		return shopPromise.get(shopConfig, '/admin/webhooks.json').
-			then(function(data) {
-				var currentHooks = data.webhooks;
-				debug(JSON.stringify(currentHooks, null, ' '));
-				var processed = [];
-				if (!makeActive) {
-					var removeHooks = currentHooks.filter(function(hk) {
-						return ~appHookTopics.indexOf(hk.topic);
-					});
-					debug('removing hooks for ' + removeHooks.map(function(hk) {
-						return hk.topic;
-					}).join(', '));
-					var removeHook = function() {
-						if (!removeHooks.length) return processed;
-						var hook = removeHooks.shift();
-						debug('remove: ' + hook.topic);
-						return shopPromise['delete'](shopConfig, '/admin/webhooks/' + hook.id + '.json').then(function() {
-							processed.push(hook);
-							return removeHook();
-						});
-					};
-					return removeHook();
-				}
-				var needHooks = appHookTopics.filter(function(ht) {
-					for (var i = 0; i < currentHooks.length; i++) {
-						if (ht == currentHooks[i].topic) return false;
-					}
-					return true;
-				});
-				if (!needHooks.length) return null;
-				debug('creating webhooks:\n\t' + needHooks.join('\n\t'));
-				var makeHooks = function() {
-					if (!needHooks.length) return processed;
-					var topic = needHooks.shift();
-					var hook = {
-						topic: topic,
-						format: 'json',
-						address: hookURL + '/' + topic
-					};
-					return shopPromise.post(shopConfig, '/admin/webhooks.json', {
-						webhook: hook
-					}).
-					then(function(data) {
-						processed.push(data.webhook);
-						return makeHooks();
-					});
-				};
-				return makeHooks();
-			}).then(function(hooks) {
-				if(!hooks) hooks = [];
-				var action = makeActive ? 'created new' : 'deleted';
-				console.log(action + ' webhooks:\n\t' + hooks.map(function(h) {
-					return h.id + ':' + h.topic;
-				}).join('\n\t'));
-				return hooks;
-			});
-	}
 
 	return {
 		appRouter : require('./lib/appRouter'),
@@ -82,7 +23,7 @@ module.exports = (function(){
 		NodeWrap : require('./lib/NodeWrap'),
 		serializeHook : require('./lib/serializeHook'),
 		session : require('./lib/shopify-ui-session'),
-		processHooks : processHooks,
+		processHooks : require('./lib/processHooks'),
 		instantPromise : instantPromise
 	};
 })();
